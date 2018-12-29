46059698fb1469ca50b6cd2fc2b4ed2a45b239e1
JustinSHong <jhong2009@gmail.com>
2018-12-28 19:48:28 -0800

    configured catalogue, searchbar, and appbar navigators

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+    createBottomTabNavigator,
+    createStackNavigator
+} from "react-navigation";
+// screens - components to be rendered by navigators
+import {
+    CatalogueScreen,
+    CategoryScreen,
+    ProductScreen,
+    SearchScreen,
+    SearchResultsScreen,
+    BagScreen
+} from "./MyScreens";
+
+const CatalogueNavigator = createStackNavigator(
+    {
+        Catalogue: CatalogueScreen,
+        Category: CategoryScreen,
+        Product: ProductScreen
+    },
+    {
+        // configuration options
+    }
+);
+
+const SearchNavigator = createStackNavigator(
+    {
+        SearchHome: SearchScreen,
+        SearchResults: SearchResultsScreen
+    },
+    {
+        // configuration options
+    }
+);
+
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        Catalogue: CatalogueNavigator,
+        Search: SearchNavigator,
+        Bag: BagScreen
+    },
+    {
+        navigationOptions: ({ navigation }) => {
+            // icons for each tab defined here
+        },
+
+        tabBarOptions: {
+            initialRouteName: "Catalogue",
+            activeTintColor: "#fff",
+            inactiveTintColor: "#ddd",
+            style: {
+                backgroundColor: "#4d535e"
+            }
+        }
+    }
+);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return <AppTabNavigator />;
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
